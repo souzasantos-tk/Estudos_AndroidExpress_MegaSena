53fb8ea37b3936525c01df44d599fe4914eb7839
souzasantos-tk <souzasantos.and@gmail.com>
2022-03-29 12:52:38 -0300

    estudos android express

---
+++ src/Mega_Sena.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Mega_Sena {
+
+    public static void main(String[] args) {
+
+        Random generate = new Random();
+
+        //while (true) vai imprimir no looping infinitos numeros.
+        //while (i<6) imprime i até 6.
+        // i++ significa que ele aumenta em um em cada looping o valo de i.
+
+        int i = 0;
+        while(i<6) {
+
+            int number = generate.nextInt(60);
+            System.out.println(number);
+            i++;
+
+            // podemos tambem usar conceito de "for" no lugar de while.
+            // for(;;) looping infinito.
+            //for( int i = 0; i<6; i++){
+            //  int number = generate.nextInt(60);
+            //         System.out.println(number);
+            // }  gerando a saida do looping.
+
+        }
+    }
+}
